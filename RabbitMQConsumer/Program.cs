becd58165d6752adda931f418a835ffc00f598b9
Ashkan <Ashkan@DESKTOP-Q7SLNT3>
2022-08-03 13:59:08 +0430

    Send A Message and Receive From RabbitMQ Queue

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory()
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare("demo-queue", durable: true, exclusive: false, autoDelete: false, null);
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, e) =>
+{
+    var body = e.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+};
+channel.BasicConsume("demo-queue", autoAck: true, consumer);
+Console.ReadKey();
---
+++ RabbitMQProducer/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using RabbitMQ.Client;
+
+
+var factory = new ConnectionFactory()
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare("demo-queue", durable: true, exclusive: false, autoDelete: false, null);
+var message=new {Name="Producer" , Message="Hello World!"};
+var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+channel.BasicPublish(exchange:"",routingKey: "demo-queue",null,body);
+  
